533d75ee9ff1ec201e4782cc129647ec6dc0442a
Elienock <lubayaelienock@gmail.com>
2023-01-25 16:40:19 +0200

    Product details and product gallery working

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from .models import Product, Variation,ReviewRating
+from .models import Product, Variation, ReviewRating, ProductGallery
+import admin_thumbnails
 
-# Register your models here.
+@admin_thumbnails.thumbnail('image')
+class ProductGalleryInline(admin.TabularInline):
+    model = ProductGallery
+    extra = 1
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_available')
     prepopulated_fields = {'slug': ('product_name',)}
-    # inlines = [ProductGalleryInline]
+    inlines = [ProductGalleryInline]
 
 class VariationAdmin(admin.ModelAdmin):
     list_display = ('product', 'variation_category', 'variation_value', 'is_active')
@@ -14,5 +18,6 @@ class VariationAdmin(admin.ModelAdmin):
     list_filter = ('product', 'variation_category', 'variation_value')
 
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Variation,VariationAdmin)
+admin.site.register(Variation, VariationAdmin)
 admin.site.register(ReviewRating)
+admin.site.register(ProductGallery)
